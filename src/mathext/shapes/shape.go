1becc1ffb2b4b1253829f3be7d4c0f7c04d084f5
Jacob Massengill <jacob_massengill@yahoo.com>
2016-09-29 16:03:11 -0400

    adding shape.go

---
+++ src/mathext/shapes/shape.go
@@ -0,0 +1,42 @@
+/*
+    Copyright (C) 2016  Jacob Massengill
+
+    length.go provides conversions between various imperial and metric units
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package shapes
+
+type GeoShape interface {
+  Area() float64
+  Perimeter() float64
+}
+
+type Shape struct{
+  Sides []float64
+  Angles []float64
+}
+
+func (s Shape) Area() float64 {
+  return 1.0
+}
+
+func (s Shape) Perimeter() float64 {
+  sum := 0
+  for _, i := range Sides{
+    sum+=i
+  }
+  return sum
+}
